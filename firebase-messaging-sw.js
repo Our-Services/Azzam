f8d2b16ab6542f7f82bf56fcf7ddbb07cd3bd908
Ahmad2039a <countries200200al@gmail.com>
2025-10-06 05:43:48 +0800

    1Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getMessaging, onBackgroundMessage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-sw.js";
+importScripts(
+    'https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js',
+    'https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js'
+);
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBMpLj1TKI4HJZeUEJ5iXekNZFv91lB1Cc",
-  authDomain: "azzam-c8aad.firebaseapp.com",
-  projectId: "azzam-c8aad",
-  storageBucket: "azzam-c8aad.firebasestorage.app",
-  messagingSenderId: "916342975413",
-  appId: "1:916342975413:web:0cd396d037eabef93b07fc",
-};
+firebase.initializeApp({
+    apiKey: "AIzaSyBMpLj1TKI4HJZeUEJ5iXekNZFv91lB1Cc",
+    authDomain: "azzam-c8aad.firebaseapp.com",
+    projectId: "azzam-c8aad",
+    storageBucket: "azzam-c8aad.firebasestorage.app",
+    messagingSenderId: "916342975413",
+    appId: "1:916342975413:web:0cd396d037eabef93b07fc"
+});
 
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+const messaging = firebase.messaging();
 
-onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: './firebase-logo.png' // Optional: you can add an icon file to your project
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+messaging.onBackgroundMessage((payload) => {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/icon.png'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
